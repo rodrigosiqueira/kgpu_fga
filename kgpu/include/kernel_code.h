f65dfb308c09b2a34c3cbc84fd2e156588085ca1
rodrigo <rodrigosiqueira>
2015-05-07 11:10:36 -0300

    Fix kernel configuration.

--- kgpu/include/kernel_code.h
+++ kgpu/include/kernel_code.h
@@ -4,8 +4,9 @@
 
 /* the NR will not be used */
 #define KGPU_BUF_NR 1
+
 // TODO: This kind of configuration should be more "automatically".
-#define KGPU_BUF_SIZE (64*64)
+#define KGPU_BUF_SIZE (128*128*128)
 
 #define KGPU_MMAP_SIZE KGPU_BUF_SIZE
 
@@ -14,10 +15,21 @@
 /* ioctl */
 #include <linux/ioctl.h>
 // TODO: Understand and comment all of this defines.
+/**
+* @def KGPU_IOC_MAGIC
+* This letter is used to identify the device driver. Kgpu is some kind of a new
+* device drive, because of this 'g' is not in the IOCTL table.
+*/
 #define KGPU_IOC_MAGIC 'g'
 
+/**
+* @def KGPU_IOC_SET_GPU_BUFS
+* @see http://www.cs.fsu.edu/~baker/devices/lxr/http/source/linux/Documentation/ioctl/ioctl-number.txt
+* First parameter is the identify, the second is the sequence number, and the
+* last one is the type of data.
+*/
 #define KGPU_IOC_SET_GPU_BUFS \
-  _IOW(KGPU_IOC_MAGIC, 1, struct kgpu_gpu_mem_info[KGPU_BUF_NR])
+  _IOW(KGPU_IOC_MAGIC, 1, struct kgpu_gpu_m:taem_info[KGPU_BUF_NR])
 
 #define KGPU_IOC_GET_GPU_BUFS \
   _IOR(KGPU_IOC_MAGIC, 2, struct kgpu_gpu_mem_info[KGPU_BUF_NR])
